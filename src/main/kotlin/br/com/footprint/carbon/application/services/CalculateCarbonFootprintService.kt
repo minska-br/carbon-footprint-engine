2627fa0218479d5313586ca61f31855ca5cc7d18
Thales Pereira <thalespereiradejesus@gmail.com>
2021-07-01 23:33:03 -0300

    Create service to request LCA information
    * Life cycle assessment or LCA is important information regarding the foods we are calculating the carbon footprint. This service is intended to communicate with the LCA API and save calculation requests in the database

---
+++ src/main/kotlin/br/com/footprint/carbon/application/services/CalculateCarbonFootprintService.kt
@@ -0,0 +1,27 @@
+package br.com.footprint.carbon.application.services
+
+import br.com.footprint.carbon.application.services.requests.RecipeRequest
+import br.com.footprint.carbon.domain.CalculationRepository
+import br.com.footprint.carbon.domain.CalculationRequest
+import br.com.footprint.carbon.domain.HttpMethod
+import br.com.footprint.carbon.infrastructure.repositories.gateways.LifeCycleAssessmentGateway
+import java.time.LocalDateTime
+import java.util.UUID
+
+class CalculateCarbonFootprintService(
+    private val lifeCycleAssessmentGateway: LifeCycleAssessmentGateway,
+    private val calculationRepository: CalculationRepository
+) {
+    suspend fun calculate(recipe: RecipeRequest): CalculationRequest {
+        lifeCycleAssessmentGateway.calculateForFoods(recipe.ingredients)
+
+        return CalculationRequest(
+            id = UUID.randomUUID().toString(),
+            href = "/calculation/requests/{request_id}",
+            method = HttpMethod.GET,
+            startTime = LocalDateTime.now().toString()
+        ).also {
+            calculationRepository.saveCalculationRequest(it)
+        }
+    }
+}
