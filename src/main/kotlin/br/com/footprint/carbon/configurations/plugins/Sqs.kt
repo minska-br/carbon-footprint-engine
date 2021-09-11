0be24bcf8a13a1bd4ea3b9232c8c97518fe46be3
Thales Pereira <thalespereiradejesus@gmail.com>
2021-09-10 22:31:56 -0300

    Create new lister to consume failed calculation events

--- src/main/kotlin/br/com/footprint/carbon/configurations/plugins/Sqs.kt
+++ src/main/kotlin/br/com/footprint/carbon/configurations/plugins/Sqs.kt
@@ -1,15 +1,17 @@
 package br.com.footprint.carbon.configurations.plugins
 
-import br.com.footprint.carbon.infrastructure.listeners.CalculationCompletedListener
+import br.com.footprint.carbon.infrastructure.listeners.CompletedCalculationListener
+import br.com.footprint.carbon.infrastructure.listeners.FailedCalculationListener
 import io.ktor.application.Application
 import kotlinx.coroutines.runBlocking
 import org.koin.ktor.ext.inject
 
 fun Application.configureSqs() {
-    val sqsSampleConsumerChannels by inject<CalculationCompletedListener>()
+    val completedCalculationListener by inject<CompletedCalculationListener>()
+    val failedCalculationListener by inject<FailedCalculationListener>()
 
     runBlocking {
-        println("${Thread.currentThread().name} Starting program")
-        sqsSampleConsumerChannels.start()
+        completedCalculationListener.start()
+        failedCalculationListener.start()
     }
 }
